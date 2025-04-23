bcc8096db1a053ba5033d2aa3ddbaec7ca5c2e09
ngoclinh2812 <hngoclinh2812@gmail.com>
2025-04-23 21:14:54 +0700

    23/04/2025: Linh remove commented code

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -3,16 +3,15 @@
 #include <cstdlib>
 #include <conio.h>
 using namespace std;
-// Kích thước khung trò chơi
-const int WIDTH = 40;    // Chiều ngang
-const int HEIGHT = 20;   // Chiều dọc
 
-// ======= HÀM DI CHUYỂN CON TRỎ TRONG CONSOLE =======
+const int WIDTH = 40;    
+const int HEIGHT = 20;  
+
 void gotoxy(int x, int y) {
     COORD coord = { SHORT(x), SHORT(y) };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
-// ======= HÀM ẨN CON TRỎ CHUỘT TRONG CONSOLE =======
+
 void HideCursor() {
 
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -21,9 +20,11 @@ void HideCursor() {
     info.bVisible = FALSE;
     SetConsoleCursorInfo(consoleHandle, &info);
 }
+
 struct Point {
     int x, y;
 };
+
 class SnakeGame {
 public:
     Point snake[100];
